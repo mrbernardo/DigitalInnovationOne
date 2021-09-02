e790144e64e608b0058424aab4440ef6f65d9d3e
Marcelo Regis Bernardo <mrbernardo@gmail.com>
2021-09-01 22:38:00 -0300

    Upload do projeto de controle de ponto

---
+++ src/main/java/one/digitalinnovation/live/model/JornadaTrabalho.java
@@ -0,0 +1,23 @@
+package one.digitalinnovation.live.model;
+
+import lombok.*;
+import org.hibernate.envers.Audited;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+@Entity
+public class JornadaTrabalho {
+    @Id
+    @GeneratedValue
+    private long id;
+    private String descricao;
+}
---
+++ src/main/java/one/digitalinnovation/live/repository/JornadaRepository.java
@@ -0,0 +1,9 @@
+package one.digitalinnovation.live.repository;
+
+import one.digitalinnovation.live.model.JornadaTrabalho;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JornadaRepository extends JpaRepository<JornadaTrabalho, Long> {
+}
